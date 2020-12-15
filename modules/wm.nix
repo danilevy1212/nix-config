62a75591074a7a47bc1babd292c8bb04c3c9ce04
dlevym <danilevy1212>
2020-12-15 21:41:15 +0100

    Fixed Dunst formatting.

--- modules/wm.nix
+++ modules/wm.nix
@@ -19,9 +19,7 @@
     settings = rec {
       global = {
         markup = "none";
-        format = ''
-          <big><b>%s</b></big>
-          %b'';
+        format = ''<big><b>%s</b></big>%b'';
         sort = false;
         alignment = "left";
         bounce_freq = 0;
